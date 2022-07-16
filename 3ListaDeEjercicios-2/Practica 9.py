29fc2880159f569ca906e134e4b9723ea0d2f51c
Oscar Mora <ozzqarmora@gmail.com>
2022-07-15 23:52:01 -0500

    ultimos cambios p9

--- 3ListaDeEjercicios-2/Practica 9.py
+++ 3ListaDeEjercicios-2/Practica 9.py
@@ -1,12 +1,36 @@
 #Practica 9 
 # 9.1_ Realice un programa que pregunte aleatoriamente una multiplicación. El programa debe indicar si la respuesta ha sido correcta o no 
 # (en caso que la respuesta sea incorrecta el programa debe indicar cúal es la correcta). El programa preguntará 10 multiplicaciones, y al finalizar mostrará el número de aciertos
-
+#def multiplicarx():
+#import random as random
+#def run():
+#    num = random.random(num.ra)
+#    numero= 
+#if __name__=='__main':
+#        run()
 #9.2_ Obtener el cuadrado de todos los elementos en la lista.
 # lista = [1,2,3,4,5,6,7,8,9,10]
 
 #9.3_ Obtener la cantidad de elementos mayores a 5 en la tupla.
-#tupla =(5,2,6,7,8,10,77,55,2,1,30,4,2,3)
+def run():
+    tupla0 =(5,2,6,7,8,10,77,55,2,1,30,4,2,3)   
+    count = 0
+    acum = 0
+    for i in tupla0:
+        if i > 5:
+            count = tupla0.count()
+            acum.append(i)
+    return contador,acum
+    print(count,acum)
+    m=(input('ingresa algo'))    
+    
+if __name__=='__main':
+    run()
 
 #9.4_ Obtener la suma de todos los elemento de la lista
-# lista = [1,2,3,4,5,6,7,8,9,10]
+#def run():    
+#    lista = [1,2,3,4,5,6,7,8,9,10]
+#    lista1= lista.sum()
+#    print(lista1)
+#if __name__=='__main':
+#    run()
