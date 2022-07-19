aeacde3cd18e7e62d8a12c9bd39e1406b9cf2cec
OSCAR <ozzqarmora@gmail.com>
2022-07-19 14:58:26 -0500

    Update Practica 9.py

--- 3ListaDeEjercicios-2/Practica 9.py
+++ 3ListaDeEjercicios-2/Practica 9.py
@@ -1,50 +1,54 @@
-#Practica 9 
-# 9.1_ Realice un programa que pregunte aleatoriamente una multiplicación. El programa debe indicar si la respuesta ha sido correcta o no 
-# (en caso que la respuesta sea incorrecta el programa debe indicar cúal es la correcta). El programa preguntará 10 multiplicaciones, y al finalizar mostrará el número de aciertos
+# practica 8
+# 8.1_ Escribe un programa python que pida un número por teclado y que cree un diccionario cuyas claves sean desde el número 1 hasta el número indicado, 
+# y los valores sean los cuadrados de las claves.
 
 #def run():
-#    import random
-#    aciertos = 0
-#    for indice in range(10):
-#        # Genero dos números aleatorios
-#        num1 = random.randint(2,10)
-#        num2 = random.randint(2,10)
-#        # Calculo la multiplicación
-#        resultado = num1*num2
-#        # Mostramos la operación de multiplicar y pedimos al usuario que
-#        # indique el resultado.
-#        print("Multiplicación ",indice)
-#        print(num1," * ",num2," = ")
-#        num_usuario = int(input())
-#        # Si acierta incrementamos el número de aciertos.
-#        if num_usuario == resultado:
-#            aciertos +=1
+#    numero = int(input("Ingrese un número:"))
+#    cuads = {}
+#
+#    for num in range(1,numero+1):
+#        cuads[num] = num ** 2  # Asignamos el valor de la key al dicc "cuads"[] en este caso num y el VALUE la op de num**num
+#    print('Se muestran los valores del 1 hasta ',numero,'Seguido de su valor al cuadrado:')
+#    for num, valor in cuads.items(): #para cada key value IN CUADS.ITEMS() se imprime
+#        print("%d -> %d" % (num,valor))
+#
+#if __name__ == '__main__':
+#    run()
+# 8.2_ Escribe un programa que lea una cadena y devuelva un diccionario con la cantidad de apariciones de cada carácter en la cadena.
+#def imprimir_items(dic):
+#    for clave,valor in dic.items():
+#        print (clave,"->",valor)
+#def run():
+#    dic = {}
+#    cadena = input("Dame una cadena:")
+#    for caracter in cadena:
+#        if caracter in dic:
+#            dic[caracter]+=1
 #        else:
-#            # Si no acierta muestro la respuesta correcta.
-#            print("No has acertado. La respuesta es ",resultado)
-#        # Mostramos el número de aciertos
-#        print("Tu nota ha sido: ",aciertos)
-#
-#if __name__=='__main__':
-#    run()
-
-#9.3_ Obtener la cantidad de elementos mayores a 5 en la tupla.                 ###INCOMPLETO
-#def run():
-#    tupla0 =(5,2,6,7,8,10,77,55,2,1,30,4,2,3)   
-#       
-#    for i in tupla0:
-#        major_five = list(filter(lambda x: x > 5, tupla0))
-#   
-#    print('Los valores de la tupla son: ',tupla0)
-#    print('Los valores mayores a 5 en la tupla son: ',major_five)
+#            dic[caracter]=1	
 #    
-#if __name__=='__main__':
-#    run()
+#    res= imprimir_items(dic)
 #
-##9.4_ Obtener la suma de todos los elemento de la lista
-#def run():    
-#    lista = [1,2,3,4,5,6,7,8,9,10]
-#    lista1= lista.sum()
-#    print(lista1)
-#if __name__=='__main__':
-#    run()
+#if __name__ == '__main__':
+#    run()    
+    
+# 8.3_ Vamos a crear un programa en python donde vamos a declarar un diccionario para guardar los precios de las distintas frutas. 
+# El programa pedirá el nombre de la fruta y la cantidad que se ha vendido y nos mostrará el precio final de la fruta a partir de los datos guardados en el diccionario. 
+# Si la fruta no existe nos dará un error. Tras cada consulta el programa nos preguntará si queremos hacer otra consulta
+#def run():
+#    precios = {"manzana": 2, "naranja": 1.5, "platano": 4, "piña": 3}
+#    while True:
+#        fruta = input("Dime la fruta que has vendido:")
+#        if fruta.lower() not in precios:
+#            print("Fruta no existe.")
+#        else:
+#            cantidad = int(input("Dime la cantidad de frutas que has vendido:"))
+#            print("El precio final es de %f" % (cantidad * precios[fruta]))
+#        opcion = input("¿Quieres vender otra fruta (s/n)")
+#        while opcion.lower() != "s" and opcion.lower() != "n":
+#            opcion = input("¿Quieres vender otra fruta (s/n)")
+#        if opcion.lower() == "n":
+#            break
+#
+#if __name__ == '__main__':
+#    run()  
